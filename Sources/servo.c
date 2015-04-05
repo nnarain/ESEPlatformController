94378c7f402c1d42c5f56ed308ff002958999e0b
Natesh Narain <nnaraindev@gmail.com>
2015-04-05 14:18:57 -0400

    docs

--- Sources/servo.c
+++ Sources/servo.c
@@ -1,4 +1,10 @@
 
+/**
+	Servo Module 
+	
+	@author Natesh Narain
+*/
+
 #include "servo.h"
 #include "timer.h"
 #include "derivative.h"
@@ -11,29 +17,38 @@
 #define DELAY_1MS  (1000 * DELAY_1US)
 #define DELAY_20MS (20 * DELAY_1MS)
 
-#define MAX 2400 // pulse with for +90
-#define MIN 600  // pulse with for -90
+#define PULSE_MAX 2400 // pulse width for +90
+#define PULSE_MIN 600  // pulse width for -90
+
+#define ANGLE_MAX 180
+#define ANGLE_MIN 0 
 
 /* Globals */
 
+// servo pulse width
 static unsigned int pulse = 0;
 
 void servo_init(void)
 {
+	// make an output compare
 	TIMER_CHNL_MAKE_OC(SERVO_CHNL);
-	TIMER_SET_OC_ACTION(SERVO_CHNL, TIMER_OC_ACTION_DRIVE_HIGH);
 	
+	// trigger the servo cycle to start in 20ms and drive high
+	TIMER_SET_OC_ACTION(SERVO_CHNL, TIMER_OC_ACTION_DRIVE_HIGH);
 	TCHNL(SERVO_CHNL) = TCNT + DELAY_20MS;
 	
+	// enable interrupts for the servo's timer channel
 	TIMER_CHNL_ENABLE_INT(SERVO_CHNL);
 }
 
 void servo_angle(unsigned int angle)
 {
-	unsigned int theta = angle % 180;
+	// limit angle to 180
+	unsigned int theta = angle % ANGLE_MAX;
 	float pulseWidth;
 	
-	pulseWidth = ((float)theta / 180.0f) * (float)(MAX + MIN);
+	// scale range [0, 180] to [600, 2400]
+	pulseWidth = ( ((angle - ANGLE_MIN) / (ANGLE_MAX - ANGLE_MIN)) * (PULSE_MAX - PULSE_MIN) ) + PULSE_MIN;
 	
 	DisableInterrupts;
 	{
@@ -50,6 +65,7 @@ interrupt SERVO_VECTOR void servo_handler(void)
 	{
 		case TIMER_OC_ACTION_DRIVE_HIGH:
 		
+			// go low after the pulse duration
 			TIMER_SET_OC_ACTION(SERVO_CHNL, TIMER_OC_ACTION_DRIVE_LOW);
 			TCHNL(SERVO_CHNL) = TCNT + pulse;
 			
@@ -59,6 +75,7 @@ interrupt SERVO_VECTOR void servo_handler(void)
 			
 		case TIMER_OC_ACTION_DRIVE_LOW:
 		
+			// go high after the 20ms period completes
 			TIMER_SET_OC_ACTION(SERVO_CHNL, TIMER_OC_ACTION_DRIVE_HIGH);
 			TCHNL(SERVO_CHNL) = TCNT + DELAY_20MS - pulse;
 			
