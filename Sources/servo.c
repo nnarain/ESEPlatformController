441eed1d792ae5e0e43af21c44728f41c498f4f0
Natesh Narain <nnaraindev@gmail.com>
2015-04-04 21:14:51 -0400

    added new servo module

---
+++ Sources/servo.c
@@ -0,0 +1,13 @@
+
+#include "servo.h"
+#include "timer.h"
+#include "derivative.h"
+
+void servo_init(void)
+{
+}
+
+void servo_angle(unsigned int angle)
+{
+
+}
---
+++ Sources/servo.h
@@ -0,0 +1,21 @@
+
+/**
+	Servo Module
+	
+	@author Natesh Narain
+*/
+
+#ifndef SERVO_H
+#define SERVO_H
+
+/**
+	Initialize the servo
+*/
+void servo_init(void);
+
+/**
+	Set the servo's angle
+*/
+void servo_angle(unsigned int angle);
+
+#endif
