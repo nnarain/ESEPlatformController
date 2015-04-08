21fd2763c6ffcacb1b3cb73bcdb93e6c7cf60d8b
Natesh Narain <nnaraindev@gmail.com>
2015-04-08 09:24:01 -0400

    update delay to use a prescaler of 8

--- Sources/delay.c
+++ Sources/delay.c
@@ -5,7 +5,7 @@ void delay_ms(word ms)
 	int i;
 	
 	// set 1 ms into future
-	TC0 = TCNT + 125;
+	TC0 = TCNT + 1000;
 	
 	// enable TCO for OC
 	TIMER_CHNL_MAKE_OC(0);
@@ -14,7 +14,7 @@ void delay_ms(word ms)
 	{
 		// wait for timer channel 0 event
 		while(!TIMER_CHNL_EVENT(0));
-		TC0 += 125;
+		TC0 += 1000;
 	}
 
 	// turn off OC on TC0
